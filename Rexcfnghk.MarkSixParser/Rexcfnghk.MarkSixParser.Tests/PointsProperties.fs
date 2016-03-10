3f8471a3977158c94a4b8007305ecf7337047410
Rex Ng <rexng@thisisnoble.com>
2016-03-10 14:51:32 +0800

    Move Points properties to another file

---
+++ Rexcfnghk.MarkSixParser/Rexcfnghk.MarkSixParser.Tests/PointsProperties.fs
@@ -0,0 +1,35 @@
+﻿module Rexcfnghk.MarkSixParser.Tests.PointsProperties
+
+open Rexcfnghk.MarkSixParser
+open Models
+open Points
+open FsCheck
+open FsCheck.Xunit
+open Swensen.Unquote
+
+[<Property>]
+let ``Points addition has identity`` d1 =
+    let p1 = Points d1
+    let p2 = Points 0.m
+    p1 .+. p2 =! p1
+
+[<Property>]
+let ``Poitns addition is communicative`` () =
+    let pointsArb = 
+        Gen.elements [1.m .. 6.m]
+        |> Gen.map Points
+        |> Arb.fromGen
+    Prop.forAll pointsArb <| fun p1 -> 
+        Prop.forAll pointsArb <| fun p2 ->
+            p1 .+. p2 =! p2 .+. p1
+
+[<Property>]
+let ``Poitns addition is associative`` () =
+    let pointsArb = 
+        Gen.elements [1.m .. 6.m]
+        |> Gen.map Points
+        |> Arb.fromGen
+    Prop.forAll pointsArb <| fun p1 -> 
+        Prop.forAll pointsArb <| fun p2 ->
+            Prop.forAll pointsArb <| fun p3 ->
+                (p1 .+. p2) .+. p3 =! p1 .+. (p2 .+. p3)
--- Rexcfnghk.MarkSixParser/Rexcfnghk.MarkSixParser.Tests/Properties.fs
+++ Rexcfnghk.MarkSixParser/Rexcfnghk.MarkSixParser.Tests/Properties.fs
@@ -3,7 +3,6 @@
 open System
 open Rexcfnghk.MarkSixParser
 open Models
-open Points
 open FsCheck
 open FsCheck.Xunit
 open Swensen.Unquote
@@ -90,30 +89,3 @@ let ``MarkSixNumber's custom equality works`` () =
         |> MarkSixNumber.create
         |> ValidationResult.extract
         =! m
-
-[<Property>]
-let ``Points addition has identity`` d1 =
-    let p1 = Points d1
-    let p2 = Points 0.m
-    p1 .+. p2 =! p1
-
-[<Property>]
-let ``Poitns addition is communicative`` () =
-    let pointsArb = 
-        Gen.elements [1.m .. 6.m]
-        |> Gen.map Points
-        |> Arb.fromGen
-    Prop.forAll pointsArb <| fun p1 -> 
-        Prop.forAll pointsArb <| fun p2 ->
-            p1 .+. p2 =! p2 .+. p1
-
-[<Property>]
-let ``Poitns addition is associative`` () =
-    let pointsArb = 
-        Gen.elements [1.m .. 6.m]
-        |> Gen.map Points
-        |> Arb.fromGen
-    Prop.forAll pointsArb <| fun p1 -> 
-        Prop.forAll pointsArb <| fun p2 ->
-            Prop.forAll pointsArb <| fun p3 ->
-                (p1 .+. p2) .+. p3 =! p1 .+. (p2 .+. p3)
