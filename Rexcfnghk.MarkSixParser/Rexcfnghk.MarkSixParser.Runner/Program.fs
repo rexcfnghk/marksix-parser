fbf69d6be198c56c04447f2c2e830a2ab4774ffe
Rex Ng <rexcfnghk@gmail.com>
2016-03-13 23:03:32 +0800

    Minor spacing fix

--- Rexcfnghk.MarkSixParser/Rexcfnghk.MarkSixParser.Runner/Program.fs
+++ Rexcfnghk.MarkSixParser/Rexcfnghk.MarkSixParser.Runner/Program.fs
@@ -5,11 +5,14 @@ open System
 [<EntryPoint>]
 let main _ = 
     printfn "Enter draw results"
+
     let drawResults = getDrawResultNumbers' ()
     printfn "The draw results are %A%s" drawResults Environment.NewLine
+
     printfn "Enter user's draw"
     let usersDraw = getUsersDrawNumbers' ()
     printfn "User's draw is %A%s" usersDraw Environment.NewLine
+
     let prize = checkResults' drawResults usersDraw
     printfn "Your prize is %A"  prize
 
