d53fe1720ae4f939f99c75d5e5977f5fd151645b
Rex Ng <rexng@thisisnoble.com>
2016-05-13 08:56:57 +0800

    Add failing unit test

---
+++ Rexcfnghk.MarkSixParser.Tests/ValidationResultFacts.fs
@@ -0,0 +1,20 @@
+﻿module Rexcfnghk.MarkSixParser.Tests.ValidationResultFacts
+
+open Swensen.Unquote
+open Xunit
+open Rexcfnghk.MarkSixParser
+
+[<Fact>]
+let ``retryable should be able to succeed eventually`` () =
+    let mutable initial = 1
+    let tester i = 
+        if i > 1 
+        then ValidationResult.success i 
+        else ValidationResult.errorFromString "Fail"
+
+    let result = ValidationResult.retryable ignore <| fun () ->
+        let attempt = tester initial
+        initial <- initial + 1
+        attempt
+
+    result >! 1
