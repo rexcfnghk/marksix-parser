d8afd7c0bee6ce696b9af117d921c8f5538895c7
Rex Ng <rexcfnghk@gmail.com>
2016-05-21 09:57:55 +0800

    Add more tests for printf

---
+++ Rexcfnghk.MarkSixParser.Tests/ValidationResultProperties.fs
@@ -0,0 +1,12 @@
+﻿module Rexcfnghk.MarkSixParser.Tests.PrintFProperties
+
+open FsCheck.Xunit
+open Rexcfnghk.MarkSixParser
+
+[<Property>]
+let ``ValidationResult %O equals to its %A representation in printf`` (v: ValidationResult<int>) =
+    sprintf "%A" v = sprintf "%O" v
+
+[<Property>]
+let ``Prize %O equals to its %A representation in printf`` (v: Prize.T) =
+    sprintf "%A" v = sprintf "%O" v
